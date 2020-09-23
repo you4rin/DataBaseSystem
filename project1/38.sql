6f25e15d1a3a1ae85378405037d7945379fd1e6b
2019027001 <artichoke42@naver.com>
2020-09-23 18:06:10 +0900

    Update project1

--- project1/38.sql
+++ project1/38.sql
@@ -1,13 +1,14 @@
 SELECT x.name
-FROM Pokemon x
+FROM Evolution a
+JOIN Pokemon x ON a.after_id=x.id
 WHERE x.id NOT IN(
   SELECT y.id 
   FROM Pokemon y
-  JOIN Evolution ON y.id=Evolution.before_id
+  JOIN Evolution b ON y.id=b.before_id
   )
 AND x.id IN(
   SELECT z.id 
   FROM Pokemon z
-  JOIN Evolution ON z.id=Evolution.after_id
+  JOIN Evolution c ON z.id=c.after_id
   )
 ORDER BY x.name ASC;
